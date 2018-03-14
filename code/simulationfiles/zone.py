45a3f611935e42e45b25cb8d87aa5aaf66a79715
Andreas Kern <andreas.kern@gmail.com>
2018-03-14 19:53:37 +0100

    [refactor] zone.py

--- code/simulationfiles/zone.py
+++ code/simulationfiles/zone.py
@@ -1,4 +1,4 @@
-import ipaddress
+from ipaddress import ip_network
 import config
 from collections import namedtuple
 
@@ -6,15 +6,20 @@ from collections import namedtuple
 class Zone:
     def __init__(self):
         self.zones = {}
-        self.counter = 0
+        self.counter = 0 # used for the ip range e.g. 240.{}.0.0/16
 
+    # return new ip address for a given latency
     def get_ip(self, latency):
+        # ip addresses with the same latency are placed in the same network
+        # if no network for a given latency exists, it is created.
         if latency not in self.zones:
-            self.counter += 1
-
-            network = ipaddress.ip_network(config.ip_zones.format(self.counter))
+            network = self._new_network_zone();
             self.zones[latency] = ZoneConfig(network, network.hosts(), latency)
         return next(self.zones[latency].hosts)
+
+    def _new_network_zone(self):
+        self.counter += 1
+        return ip_network(config.ip_zones.format(self.counter))
 
 
 ZoneConfig = namedtuple('ZoneConfig', 'network hosts latency')
