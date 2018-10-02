d99e0f6cfe5d84b9048a75fd22ca0dcf1b05e2df
Andreas Kern <andreas.kern@gmail.com>
2018-10-02 16:32:01 +0200

    add whitelist to bitcoind

--- code/cmd/bitcoincmd.py
+++ code/cmd/bitcoincmd.py
@@ -6,6 +6,9 @@ args = {
     'regtest':            '-regtest',
     'datadir':            '-datadir=' + config.bitcoin_data_dir,
 
+    # disable security checks for better performance
+    'whitelist': f'-whitelist={config.ip_range}',
+
     # log all events relevant for parsing
     'debug':              '-debug=cmpctblock -debug=net -debug=mempool',
     'logips':             '-logips',
@@ -32,15 +35,14 @@ args = {
 
 def start(name, ip, docker_image, path, connect_to_ips):
     return_args = args.copy()
-    cmd = transform_to_cmd(return_args)
+    cmd = daemon + ' '.join((return_args).values())
     for _ip in connect_to_ips:
         cmd += ' -connect=' + str(_ip)
     return dockercmd.run_node(name, ip, docker_image, cmd, path)
 
 
-def transform_to_cmd(args_to_transform):
-    return daemon + ' '.join(args_to_transform.values())
-
-
 def rm_peers(node):
     return dockercmd.exec_cmd(node, 'rm -f {}/regtest/peers.dat'.format(config.bitcoin_data_dir))
+
+if __name__ == "__main__":
+    print(daemon + ' '.join(args.values()))
